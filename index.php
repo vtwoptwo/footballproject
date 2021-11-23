58dcb021f9c73ca807c0de767ebbc55a5556d782
talphaidze <94868249+talphaidze@users.noreply.github.com>
2021-11-23 19:52:40 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Site Index</title>
+	<link rel="stylesheet" type ="text/css" href="mystyle.css">
+</head>
+<body>
+<h1>Site Index</h1>
+<p>Please Enter Your Username and Password</p>
+<ul>
+	<li><a href = "userform1.html">SIGN IN</a></li>
+</ul>
+</body>
+</html>
