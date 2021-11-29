2c61f0627dd4d0c780c4fdc8355407445aef184b
vtwoptwo <84085010+vtwoptwo@users.noreply.github.com>
2021-11-29 02:48:36 +0100

    update

--- index.php
+++ index.php
@@ -8,33 +8,41 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,200;0,500;0,700;1,900&display=swap" rel="stylesheet">
 <body>
-<body>
 <section class="header">
     <nav>
         <a href="index.php"><img src="images/logo.PNG"></a>
         <div class="navbar"></div>
             <ul>
-                <li><a href = "">Home</a></li>
-                <li><a href = "">About</a></li>
-                <li><a href = "">Organizations</a></li>
-                <li><a href = "">Contact</a></li>
-                <li><a href = "login.php">LogIn</a></li>
+                <li><a href = "index.php">Home</a></li>
+                <li><a href = "login_owner.php">Owner</a></li>
+                <li><a href = "login_coach.php">Coach</a></li>
+                <li><a href = "login_manager.php">Manager</a></li>
+                <li><a href = "login_player.php">Player</a></li>
+                
             </ul>
     </nav>
 
 <div class="textbox">
     <h1>Best Football Team Ever </h1>
-    <p>This is some random text</p>
+    <p>Welcome to our official homepage!</p>
     <a href="login.php" class="hp-btn"> LogIn to Access Your Data </a>
 </div>
 
-    
 </section>
 
 <section class="info">
     <h1> With Footloose you learn how to fly! </h1>
     <p>lorem ipsum bla bla bla</p>
+
+    <div class="class"></div>
 </section>
-     
+
+
+<section class="contact">
+    <h1> Contact </h1>
+    <p>lorem ipsum bla bla bla</p>
+
+    <div class="class"></div>
+</section>
 </body>
 </html>
